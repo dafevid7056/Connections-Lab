8dd4660c16a95a6324115135e2d335f7638edd6a
David Felipe Rodríguez <dfr7056@nyu.edu>
2025-09-15 21:57:38 -0500

    Added comments and disabled the puke meds at the beginning.

--- Week_2_Two_Sentence_Stories_2/script.js
+++ Week_2_Two_Sentence_Stories_2/script.js
@@ -1,8 +1,19 @@
+// initialize variables to reference HTML elements
 let meds = document.getElementById("takemeds");
 let puke = document.getElementById("pukemeds");
 let alpha = document.getElementById("character");
 let conclusion = document.getElementById("conclusion");
 
+// the take meds button is disabled at the start
+if (alpha) {
+    alpha.style.opacity = "1";
+}
+if (puke) {
+    puke.disabled = true;
+}
+
+// set initial opacity of the mother character to fully visible
+// the meds button make the mother more transparent
 meds.addEventListener("click", function () {
     console.log("The meds button has been clicked!");
     if (alpha) {
@@ -18,7 +29,7 @@ meds.addEventListener("click", function () {
         puke.disabled = false;
     }
 });
-
+// the puke button makes the mother more visible
 puke.addEventListener("click", function () {
     console.log("The puke button has been clicked!");
     if (alpha) {
